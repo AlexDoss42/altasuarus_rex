dbe66e3fc6a1c4411d461ae8944b9586d539a68d
AlexDoss42 <alex.doss42@gmail.com>
2020-02-24 18:11:30 -0700

    Init Commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'altasuarus_rex',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
